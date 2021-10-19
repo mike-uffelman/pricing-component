ab20ca51dbdd222b16eb68dcd45bdd6e56db6c73
mike-uffelman <muffelma@gmail.com>
2021-10-19 10:11:17 -0400

    inputs working

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-// const priceDisplay = document.querySelector('#price-display');
-// const slider = document.querySelector('#slider');
-// const form = document.querySelector('form');
+const priceDisplay = document.querySelector('#price-display');
+const pageViews = document.querySelector('.card__header--pageviews');
+const slider = document.querySelector('#slider');
+const form = document.querySelector('form');
+const dataListLabel = document.querySelector('#pageviews');
 
-// slider.addEventListener('change', () => {
-//     console.log(slider.value)
+pageViews.innerText = 'hi';
+// priceDisplay = 
+
+
+
+class App {
+
+    constructor() {
+    form.addEventListener('input', this.setSelection)
+
+    }
+
+    setSelection() {
+        const yearlyBilling = document.querySelector('#billing').checked;
+        const discount = .75;
+        const sliderValue = slider.value;
+        
+        if(!sliderValue) return;
+        
+        const option = document.getElementsByTagName('option')[sliderValue]
+    
+        //set pageview text display based on option
+        pageViews.innerText = `${option.label}`
+    
+        //if yearly billing checked apply discount otherwise disply full price
+        if(yearlyBilling === true) {
+            priceDisplay.innerText = `$ ${Number(option.value) * discount}.00/month`
+        } else {
+            priceDisplay.innerText = `$ ${Number(option.value)}.00/month`
+        }  
+    }
     
     
-//     priceDisplay.innerText = `$${slider.value}.00/month`;
+    
+    
+    
 
-// })
-
-const priceThumb = document.querySelector('.input__range--thumb');
-
-priceThumb.oninput = () => {
-    console.log('inputting...')
 }
 
-console.log(priceThumb.ondragleave) 
+const app = new App();
