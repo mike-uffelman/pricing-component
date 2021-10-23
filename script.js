cd6d78e0a75d3415ec7e80c864e62d8ce2b04cbb
mike-uffelman <muffelma@gmail.com>
2021-10-22 23:48:50 -0400

    upload layoutTest version

--- script.js
+++ script.js
@@ -1,68 +1,36 @@
 const priceDisplay = document.querySelector('#price__display--amount');
-const pageViews = document.querySelector('.card__header--pageviews');
+const pageViews = document.querySelector('.card--pageviews');
 const slider = document.querySelector('#slider');
 const form = document.querySelector('form');
+const billingSwitch = document.querySelector('input[type=checkbox]');
+
 
 class Pricing {
+    #discount = .75;
 
     constructor() {
         this._defaultSelection();
-        form.addEventListener('input', this._setSelection)
+        this._rangeProgress();
+        form.addEventListener('input', this._setSelection.bind(this));
+        slider.addEventListener('input', this._rangeProgress);
+        billingSwitch.addEventListener('click', this._toggleBilling);
     }
 
+    //displays the pageviews and cost amount at the default on page load
     _defaultSelection() {
         const sliderValue = slider.value;
-
-        // test attempt to create proximity snapping on the input
-        // let sliderValue;
-
-        // if(slider.value <= 100) {
-        //     sliderValue = 0;
-        // } else if(slider.value > 100 && slider.value <= 200) {
-        //     sliderValue = 1;
-        // } else if(slider.value > 200 && slider.value <= 300) {
-        //     sliderValue = 2;
-        // } else if(slider.value > 300 && slider.value <= 400) {
-        //     sliderValue = 3;
-        // } else if(slider.value > 400) {
-        //     sliderValue = 4;
-        // }
-
-
         const option = document.getElementsByTagName('option')[sliderValue]
         
         priceDisplay.innerText = `$${Number(option.value)}.00`
         pageViews.innerText = `${option.label}`;
-
-
-
-
-
     }
 
+    //sets the pageview and monthly cost based on the input range position, also applies the discount if the switch is toggled
     _setSelection() {
         const yearlyBilling = document.querySelector('#billing').checked;
-        const discount = .75;
         const sliderValue = slider.value;
-        // let sliderValue;
-
-        console.log(slider.value);
 
         if(!sliderValue) return;
-
-        // if(slider.value <= 100) {
-        //     sliderValue = 0;
-        //     // document.getElementsByTagName('input[type=range]').value = 0;
-
-        // } else if(slider.value > 100 && slider.value <= 200) {
-        //     sliderValue = 1;
-        // } else if(slider.value > 200 && slider.value <= 300) {
-        //     sliderValue = 2;
-        // } else if(slider.value > 300 && slider.value <= 400) {
-        //     sliderValue = 3;
-        // } else if(slider.value > 400) {
-        //     sliderValue = 4;
-        // }
 
         const option = document.getElementsByTagName('option')[sliderValue]
 
@@ -71,11 +39,29 @@ class Pricing {
     
         //if yearly billing checked apply discount otherwise disply full price
         if(yearlyBilling === true) {
-            priceDisplay.innerText = `$${Number(option.value) * discount}.00`
+            priceDisplay.innerText = `$${Number(option.value) * this.#discount}.00`
         } else {
             priceDisplay.innerText = `$${Number(option.value)}.00`
         }  
     }
+
+    //adds progress background color to the range track
+    _rangeProgress() {
+        const progressColor = getComputedStyle(document.body).getPropertyValue('--soft-cyan');
+
+        slider.style.background = `linear-gradient(to right, ${progressColor} ${slider.value * 25}%, ${progressColor} ${slider.value * 25}%, var(--light-grayish-blue-slider) ${slider.value *25 }%)`
+    }
+
+    //add text color to the billing timing based on the switch toggle
+    _toggleBilling() {
+            document.querySelector('.yearly').classList.toggle('selected');
+            document.querySelector('.monthly').classList.toggle('selected');
+    }
+    
+
+
 }
 
+//executes the Pricing class
 const pricing = new Pricing();
+
